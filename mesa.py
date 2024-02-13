a57015afcd20b6ff339076ca44a92e4c4d735621
caiomp <caiomp@ufba.br>
2024-02-12 22:44:01 -0300

    Objeto que representa a mesa de rouleta

---
+++ mesa.py
@@ -0,0 +1,5 @@
+class Mesa:
+    def __init__(self, nome, ultimos_resultados, nome_crupie):
+        self.nome = nome
+        self.ultimos_resultados = ultimos_resultados
+        self.nome_crupie = nome_crupie
