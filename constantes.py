000e2b66cd763849646bd8fb7d24bec4a25233e7
caiomp <caiomp@ufba.br>
2024-02-14 17:03:35 -0300

    Deixando o LOGIN e a SENHA genérico

--- constantes.py
+++ constantes.py
@@ -1,5 +1,5 @@
-LOGIN = "wuillker14@gmail.com"
-SENHA = "Minhapica!1"
+LOGIN = "exemplo@gmail.com"
+SENHA = "teste123"
 QUANTIDADE_PADRAO = 6
 NUMEROS_PARES = [str(num) for num in [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36]]
 NUMEROS_IMPARES = [str(num) for num in [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]]
