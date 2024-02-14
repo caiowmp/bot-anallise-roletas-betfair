8299e33ee8b2981fa7dc836bc59164cb193e960b
caiomp <caiomp@ufba.br>
2024-02-14 12:03:29 -0300

    Mudando a forma de verificar os padrões

--- mesa.py
+++ mesa.py
@@ -22,17 +22,18 @@ class Mesa:
 
     @property
     def ultimos_resultados(self):
+        while (len(self._ultimos_resultados) > constantes.QUANTIDADE_PADRAO):
+            self._ultimos_resultados.pop()
         return self._ultimos_resultados
 
     @ultimos_resultados.setter
     def ultimos_resultados(self, value):
         self._ultimos_resultados = value
-        while (len(self._ultimos_resultados) > constantes.QUANTIDADE_PADRAO):
-            self._ultimos_resultados.pop()
 
     @property
     def nome_croupier(self):
         return self._nome_croupier
+
 
     @nome_croupier.setter
     def nome_croupier(self, value):
@@ -40,24 +41,66 @@ class Mesa:
 
     def __str__(self):
         return f"Mesa: {self.nome}, Últimos Resultados: {self.ultimos_resultados}, Crupiê: {self.nome_crupie}"
-    
+
     def verificar_padrao(self):
         resultado = ''
-        if self.ultimos_resultados in constantes.NUMEROS_VERMELHOS:
+        if self.verificar_padrao_vermelho():
             resultado += 'Números Vermelhos\n'
-        if self.ultimos_resultados in constantes.NUMEROS_PRETOS:
+        if self.verificar_padrao_preto():
             resultado += 'Números Pretos\n'
-        if self.ultimos_resultados in constantes.NUMEROS_PARES:
+        if self.verificar_padrao_par():
             resultado += 'Números Pares\n'
-        if self.ultimos_resultados in constantes.NUMEROS_IMPARES:
+        if self.verificar_padrao_impar():
             resultado += 'Números Impares\n'
-        if self.ultimos_resultados in constantes.NUMEROS_ALTOS:
+        if self.verificar_padrao_alto():
             resultado += 'Números Altos\n'
-        if self.ultimos_resultados in constantes.NUMEORS_BAIXOS:
+        if self.verificar_padrao_baixo():
             resultado += 'Números Baixos\n'
         if resultado != '':
             self.notificar(resultado)
 
+    def verificar_padrao_vermelho(self):
+        contador = 0
+        for numero in self.ultimos_resultados:
+            if numero in constantes.NUMEROS_VERMELHOS:
+                contador += 1
+        return contador == constantes.QUANTIDADE_PADRAO
+    
+    def verificar_padrao_preto(self):
+        contador = 0
+        for numero in self.ultimos_resultados:
+            if numero in constantes.NUMEROS_PRETOS:
+                contador += 1
+        return contador == constantes.QUANTIDADE_PADRAO
+    
+    def verificar_padrao_par(self):
+        contador = 0
+        for numero in self.ultimos_resultados:
+            if numero in constantes.NUMEROS_PARES:
+                contador += 1
+        return contador == constantes.QUANTIDADE_PADRAO
+    
+    def verificar_padrao_impar(self):
+        contador = 0
+        for numero in self.ultimos_resultados:
+            if numero in constantes.NUMEROS_IMPARES:
+                contador += 1
+        return contador == constantes.QUANTIDADE_PADRAO
+    
+    def verificar_padrao_baixo(self):
+        contador = 0
+        for numero in self.ultimos_resultados:
+            if numero in constantes.NUMEORS_BAIXOS:
+                contador += 1
+        return contador == constantes.QUANTIDADE_PADRAO
+    
+    def verificar_padrao_alto(self):
+        contador = 0
+        for numero in self.ultimos_resultados:
+            if numero in constantes.NUMEROS_ALTOS:
+                contador += 1
+        return contador == constantes.QUANTIDADE_PADRAO
+            
     def notificar(self, entrada: str):
         notificacao = Notification(app_id="Bot BetFair", 
                              title="Entrada Encontrada", 
